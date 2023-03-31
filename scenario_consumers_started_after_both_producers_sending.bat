04cddc9e3e3948c3befb7dff3a039088c39a7042
Sandlo <128033399+Sandlo@users.noreply.github.com>
2023-03-31 16:29:11 +0200

    Add files via upload

---
+++ scenario_consumers_started_after_both_producers_sending.bat
@@ -0,0 +1,8 @@
+docker-compose exec java_smartphone_app java Main.java sendeHardwareBestellung1
+docker-compose exec java_smartphone_app java Main.java sendeHardwareBestellung2
+docker-compose exec java_smartphone_app java Main.java sendeSoftwareBestellung1
+docker-compose exec java_smartphone_app java Main.java sendeSoftwareBestellung2
+docker-compose exec javascript_website node main.js sendSoftwareOrder
+docker-compose exec javascript_website node main.js sendHardwareOrder
+start cmd /k docker-compose exec java_hardware_order java Main start_listening
+start cmd /k docker-compose exec javascript_software_order node main.js listen
